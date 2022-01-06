50f71be692fa3b37cb36a60bf5f1369d758571e1
Dat Tran <tdat1@tma.com.vn>
2022-01-06 14:13:12 +0700

    fix(backend): deleted apache kafka

--- src/main/java/vn/onltest/event/constant/ActionConstants.java
+++
@@ -1,8 +0,0 @@
-package vn.onltest.event.constant;
-
-public class ActionConstants {
-    public static final String GET = "GET";
-    public static final String CREATED = "CREATED";
-    public static final String UPDATED = "UPDATED";
-    public static final String DELETED = "DELETED";
-}
